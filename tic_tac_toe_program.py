af21bcf9ee8af0857b93fe76424c3a33fa7c7a43
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2021-03-23 13:12:12 +0530

    [Sohail]Added : Drawing board and Resetting a board

--- tic_tac_toe_program.py
+++ tic_tac_toe_program.py
@@ -1,3 +1,25 @@
+import random
+
+
+def drawBoard(board):
+    """
+    This function prints out the board that it was passed.
+    "board" is a list of 10 strings representing the board (ignore index 0)
+    :param board:
+    :return:
+    """
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+
+
 while True:
     # Reset the board
     theBoard = [' '] * 10
